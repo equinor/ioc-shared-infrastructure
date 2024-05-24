45bf4d3d650d257d0e6935ccd4b3269b24acbd73
fsat <fsat@equinor.com>
2024-05-24 12:39:30 +0200

    rename scripts. add FORCE-option to make publish. update snyk-infrastructure

--- scripts/bicep_versions.sh
+++ scripts/bicep_versions.sh
@@ -3,14 +3,14 @@
 rootPath=$1
 
 prefix="$rootPath/resources/"
-if [ -f $rootPath/BICEP_CHANGELOG.md ]; then
-    rm $rootPath/BICEP_CHANGELOG.md
+if [ -f $rootPath/BICEP_RESOURCE_VERSIONS.md ]; then
+    rm $rootPath/BICEP_RESOURCE_VERSIONS.md
 fi
 
 now=$(date '+%Y-%m-%d %H:%M:%S')
 
-touch $rootPath/BICEP_CHANGELOG.md
-echo "# Bicep Change Log $now" >> $rootPath/BICEP_CHANGELOG.md
+touch $rootPath/BICEP_RESOURCE_VERSIONS.md
+echo "# Bicep Change Log $now" >> $rootPath/BICEP_RESOURCE_VERSIONS.md
 for dir in $rootPath/resources/*
 do
     test -d $dir || continue
@@ -21,5 +21,5 @@ do
         line="Version not found in file"
     fi
     file=${file#"$prefix"}
-    printf "%-60s $line\n" $file  >> $rootPath/BICEP_CHANGELOG.md
+    printf "%-60s $line\n" $file  >> $rootPath/BICEP_RESOURCE_VERSIONS.md
 done
